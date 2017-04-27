1236236b44b3eb2e3e52dd6ff544e3172b037e22
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-04-27 19:51:07 -0400

    some guys on the internet say tenth percentile is sufficient though I don't know if I believe it

--- attack-client.py
+++ attack-client.py
@@ -11,7 +11,7 @@ needle = b"this is very secret you cannot guess"
 def main(host, port):
     reporter = Reporter(time())
     guesser = Guesser(
-        len(needle), 100,
+        len(needle), 50,
     )
     s = socket()
     s.connect((host, int(port)))
@@ -128,43 +128,51 @@ class Guesser(object):
 
 
     def _analyse(self, all_samples, measurements):
-        ttest = []
-        for candidate, some_samples in measurements.iteritems():
-            result = ttest_ind(some_samples, all_samples)
-            ttest.append((result.pvalue, result.statistic, candidate))
+        def tenth(samples):
+            return samples[int(len(samples) * 0.9)]
 
-        hypothesis_rejected = list(
-            (pvalue, statistic, candidate)
-            for (pvalue, statistic, candidate)
-            in ttest
-            # Any p-value less than our threshhold is a rejection of the null
-            # hypothesis, which is that the samples are drawn from the sample
-            # population (the server responded to the candidate in the same
-            # amount of time as it required for all the other candidates).
-            if pvalue < self.significance
-            # Any statistic greater than zero means the server took longer to
-            # respond to the candidate than it took to respond on average
-            # across all candidates.  This means it did some more work - like
-            # comparing the additional candidate byte. XXX Why compare against
-            # value even greater than 0 here?
-            and statistic > self.minimum_mean_difference
-        )
+        candidates = sorted(measurements.iteritems(), key=lambda (k, v): tenth(sorted(v)))
+        return candidates[-1][0]
 
-        if len(hypothesis_rejected) == 1:
-            [(pvalue, statistic, candidate)] = hypothesis_rejected
-            print("Chose {!r} with p-value {} (differs by {}).".format(candidate, pvalue, statistic))
-            return candidate
-        elif len(hypothesis_rejected) > 1:
-            print("Rejected hypothesis for several candidates: {!r}.".format(hypothesis_rejected))
-        elif len(hypothesis_rejected) < 1:
-            print(
-                "Failed to reject the hypothesis at {} sample level "
-                "(closest: p-value {}; difference {}; candidate: {}".format(
-                    len(all_samples) / len(measurements),
-                    *min(ttest)
-                )
-            )
-        return None
+
+    # def _analyse(self, all_samples, measurements):
+    #     ttest = []
+    #     for candidate, some_samples in measurements.iteritems():
+    #         result = ttest_ind(some_samples, all_samples)
+    #         ttest.append((result.pvalue, result.statistic, candidate))
+
+    #     hypothesis_rejected = list(
+    #         (pvalue, statistic, candidate)
+    #         for (pvalue, statistic, candidate)
+    #         in ttest
+    #         # Any p-value less than our threshhold is a rejection of the null
+    #         # hypothesis, which is that the samples are drawn from the sample
+    #         # population (the server responded to the candidate in the same
+    #         # amount of time as it required for all the other candidates).
+    #         if pvalue < self.significance
+    #         # Any statistic greater than zero means the server took longer to
+    #         # respond to the candidate than it took to respond on average
+    #         # across all candidates.  This means it did some more work - like
+    #         # comparing the additional candidate byte. XXX Why compare against
+    #         # value even greater than 0 here?
+    #         and statistic > self.minimum_mean_difference
+    #     )
+
+    #     if len(hypothesis_rejected) == 1:
+    #         [(pvalue, statistic, candidate)] = hypothesis_rejected
+    #         print("Chose {!r} with p-value {} (differs by {}).".format(candidate, pvalue, statistic))
+    #         return candidate
+    #     elif len(hypothesis_rejected) > 1:
+    #         print("Rejected hypothesis for several candidates: {!r}.".format(hypothesis_rejected))
+    #     elif len(hypothesis_rejected) < 1:
+    #         print(
+    #             "Failed to reject the hypothesis at {} sample level "
+    #             "(closest: p-value {}; difference {}; candidate: {}".format(
+    #                 len(all_samples) / len(measurements),
+    #                 *min(ttest)
+    #             )
+    #         )
+    #     return None
 
 
 
